7b4df77718d7b67a6c3d949e6a39bfae6f63f2b0
Youngmin Cho <choyoungmin0322@gmail.com>
2025-02-09 21:54:03 +0900

    코드로 label 텍스트 그리기

--- UIKit Study/ViewController.swift
+++ UIKit Study/ViewController.swift
@@ -7,10 +7,31 @@
 
 import UIKit
 
+#Preview {
+    ViewController()
+}
+
 class ViewController: UIViewController {
+    
+    lazy var myNameTextLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "피피아노"
+        label.textColor = .white
+        label.textAlignment = .center
+        label.backgroundColor = .black
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .orange
+        
+        view.addSubview(myNameTextLabel)
+        myNameTextLabel.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        myNameTextLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        myNameTextLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        myNameTextLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 
 
