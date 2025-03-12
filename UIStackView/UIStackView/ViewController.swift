f214025ee3e26d0f5943075e5c01902cffc8f74d
Youngmin Cho <choyoungmin0322@gmail.com>
2025-03-13 00:24:32 +0900

    스토리보드 삭제 및 다크모드 라이트모드 기능 적용 입력결과 출력용 라벨 텍스트 추가

--- UIStackView/UIStackView/ViewController.swift
+++ UIStackView/UIStackView/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
 
+    var textField: UITextField!
+    private var cancellables = Set<AnyCancellable>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textField = UITextField()
+        textField.placeholder = "Enter text"
+        textField.borderStyle = .roundedRect
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(textField)
+        
+        NSLayoutConstraint.activate([
+            textField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            textField.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
     }
-
-
 }
 
