9667fbc6157b6899f9ceacac5d88b1917722a63b
Youngmin Cho <choyoungmin0322@gmail.com>
2025-03-13 00:22:49 +0900

    스토리보드 삭제 및 다크모드 라이트모드 기능 적용 입력결과 출력용 라벨 텍스트 추가

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -8,79 +8,79 @@
 import UIKit
 
 class ViewController: UIViewController, SecondViewControllerDelegate {
-    
-    // lazy var: 초기화를 늦게 하기 위해 사용하는 키워드
-    lazy var myLabel: UILabel = {
-        print("myLabel 생성")
-        let label = UILabel()
-        label.text = "입력 결과를 출력합니다."
-        label.textAlignment = .center
-        label.textColor = .black
-        label.font = UIFont.systemFont(ofSize: 24)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("01 ViewController.viewDidLoad()")
-        setupUI()
-    }
-    
-    func setupUI() {
-        let label = UILabel()
-        label.text = "Hello, World!"
-        // (content layout) 라벨의 텍스트를 가운데 정렬
-        label.textAlignment = .center
-        label.textColor = .black
-        label.font = UIFont.systemFont(ofSize: 24)
-        // (frame layout) 붙여질 뷰의 위치와 크기를 설정
-        label.translatesAutoresizingMaskIntoConstraints = false
-        // self.view(메인 뷰)에 라벨을 서브 뷰로 추가
-        self.view.addSubview(label)
-        
-        NSLayoutConstraint.activate([
-            label.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100),
-            label.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20),
-            label.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20),
-            label.heightAnchor.constraint(equalToConstant: 40)
-        ])
-        
-        // 버튼 추가
-        
-        let button = UIButton()
-        button.setTitle("Go Second", for: .normal)
-        button.setTitleColor(.blue, for: .normal)
-        button.addTarget(self, action: #selector(goSecond), for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        
-        self.view.addSubview(button)
-        
-        NSLayoutConstraint.activate([
-            button.topAnchor.constraint(equalTo: label.topAnchor, constant: 100),
-            button.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20),
-            button.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20),
-            button.heightAnchor.constraint(equalToConstant: 40)
-        ])
-        
-        self.view.addSubview(myLabel)
-        
-        NSLayoutConstraint.activate([
-            myLabel.topAnchor.constraint(equalTo: button.topAnchor, constant: 100),
-            myLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20),
-            myLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20),
-            myLabel.heightAnchor.constraint(equalToConstant: 40)
-        ])
-    }
-    
-    func didDismissSecondViewController(message: String) {
-        myLabel.text = message
-    }
-    
-    
-    @objc func goSecond() {
-        let secondVC = SecondViewController()
-        self.present(secondVC, animated: true)
-    }
+
+  // lazy var: 초기화를 늦게 하기 위해 사용하는 키워드
+  lazy var myLabel: UILabel = {
+    print("myLabel 생성")
+    let label = UILabel()
+    label.text = "입력 결과를 출력합니다."
+    label.textAlignment = .center
+    label.textColor = .black
+    label.font = UIFont.systemFont(ofSize: 24)
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    print("01 ViewController.viewDidLoad()")
+    setupUI()
+  }
+
+  func setupUI() {
+    let label = UILabel()
+    label.text = "Hello, World!"
+    // (content layout) 라벨의 텍스트를 가운데 정렬
+    label.textAlignment = .center
+    label.textColor = .black
+    label.font = UIFont.systemFont(ofSize: 24)
+    // autolayout 설정
+    label.translatesAutoresizingMaskIntoConstraints = false
+    // self.view(메인 뷰)에 라벨을 서브 뷰로 추가
+    self.view.addSubview(label)
+
+    NSLayoutConstraint.activate([
+      label.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100),
+      label.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20),
+      label.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20),
+      label.heightAnchor.constraint(equalToConstant: 40)
+    ])
+
+    // 버튼 추가
+
+    let button = UIButton()
+    button.setTitle("Go Second", for: .normal)
+    button.setTitleColor(.blue, for: .normal)
+    button.addTarget(self, action: #selector(goSecond), for: .touchUpInside)
+    button.translatesAutoresizingMaskIntoConstraints = false
+
+    self.view.addSubview(button)
+
+    NSLayoutConstraint.activate([
+      button.topAnchor.constraint(equalTo: label.topAnchor, constant: 100),
+      button.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20),
+      button.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20),
+      button.heightAnchor.constraint(equalToConstant: 40)
+    ])
+
+    self.view.addSubview(myLabel)
+
+    NSLayoutConstraint.activate([
+      myLabel.topAnchor.constraint(equalTo: button.topAnchor, constant: 100),
+      myLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20),
+      myLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20),
+      myLabel.heightAnchor.constraint(equalToConstant: 40)
+    ])
+  }
+
+  func didDismissSecondViewController(message: String) {
+      myLabel.text = message
+  }
+
+  @objc func goSecond() {
+    let secondVC = SecondViewController()
+    secondVC.delegate = self
+    self.present(secondVC, animated: true)
+  }
 }
---
+++ UIStackView/UIStackView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIStackView
+//
+//  Created by 조영민 on 3/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
