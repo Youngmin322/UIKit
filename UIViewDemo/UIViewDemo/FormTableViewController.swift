e261956e4157e29b1eaea0faec7fd9e7d9250a33
Youngmin Cho <choyoungmin0322@gmail.com>
2025-03-13 14:17:11 +0900

    테이블 뷰 셀 구성 개선: UIListContentConfiguration을 사용하여 셀의 텍스트 설정

--- UIViewDemo/UIViewDemo/FormTableViewController.swift
+++ UIViewDemo/UIViewDemo/FormTableViewController.swift
@@ -7,23 +7,56 @@
 
 import UIKit
 
-class FormTableViewController: UIViewController {
+class FormTableViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.tableView.separatorColor = .black
+  }
 
-        // Do any additional setup after loading the view.
+  // MARK: - Table view data source
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 4
+  }
+
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // #warning Incomplete implementation, return the number of rows
+    switch section {
+    case 0:
+      return 1
+    case 3:
+      return 3
+    default:
+      return 2
     }
-    
+  }
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    // 테이블 뷰 셀을 재사용 하거나 새로 생성
+    let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+
+      var config = UIListContentConfiguration.subtitleCell()
+      config.text = "Section: \(indexPath.section), Row: \(indexPath.row)"
+      cell.contentConfiguration = config
+
+    return cell
+  }
+
+  override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    switch indexPath.section {
+    case 0:
+      return 100
+    case 1:
+      return 200
+    default:
+      return 50
     }
-    */
 
+  }
+
+}
+
+#Preview {
+  FormTableViewController(style: .insetGrouped)
 }
