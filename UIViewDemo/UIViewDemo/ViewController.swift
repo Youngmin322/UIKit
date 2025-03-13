835d2f686c528a9ae636a5cb33220bd48b381a6f
Youngmin Cho <choyoungmin0322@gmail.com>
2025-03-13 11:24:47 +0900

    UIStackView 추가

---
+++ UIViewDemo/UIViewDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIViewDemo
+//
+//  Created by 조영민 on 3/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
