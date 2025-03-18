d9f57ee3591dda78020bda1bb72d7c5c6436426e
Youngmin Cho <choyoungmin0322@gmail.com>
2025-03-18 10:26:39 +0900

    UI 개선: 버튼 제목을 '메뉴 열기'로 변경하고, 메뉴 선택 기능 추가

--- MenuDemo/MenuDemo/ViewController.swift
+++ MenuDemo/MenuDemo/ViewController.swift
@@ -11,23 +11,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupUI()
     }
     
     func setupUI() {
-        
         // 디폴트 버튼 설정
         var config = UIButton.Configuration.plain()
-        config.title = "Apple"
-        config.baseBackgroundColor = .systemBlue
         
         // 밑줄 스타일
-        let attributedTitle = AttributedString("String")
+        let attributedTitle = AttributedString("메뉴 열기")
         var container = AttributeContainer()
-        container.underlineStyle = .single
+        container.underlineStyle = [.single]
         container.underlineColor = .systemBlue
-        container.font = .systemFont(ofSize: 20)
+        container.font = .systemFont(ofSize: 50)
         
         let underlinedTitle = attributedTitle.settingAttributes(container)
         config.attributedTitle = underlinedTitle
@@ -36,7 +32,7 @@ class ViewController: UIViewController {
         let button = UIButton(configuration: config)
         button.addAction(UIAction { [weak self] _ in
             print("버튼 클릭")
-            self?.openLink()
+            self?.showMenu()
         }, for: .touchUpInside)
         
         button.translatesAutoresizingMaskIntoConstraints = false
@@ -50,14 +46,56 @@ class ViewController: UIViewController {
         ])
     }
     
+    func showMenu() {
+        let alert = UIAlertController(title: "메뉴", message: "메뉴를 선택하세요", preferredStyle: .actionSheet)
+        
+        // 클로저를 직접 사용하는 방식으로 변경
+        alert.addAction(UIAlertAction(title: "Open", style: .default) { [weak self] _ in
+            self?.showMessage("Open 선택됨")
+        })
+        
+        alert.addAction(UIAlertAction(title: "Find", style: .default) { [weak self] _ in
+            self?.showMessage("Find 선택됨")
+        })
+        
+        alert.addAction(UIAlertAction(title: "Delete", style: .destructive) { [weak self] _ in
+            self?.showMessage("Delete 선택됨")
+        })
+        
+        // 아이패드에서 팝오버 형태로 메뉴를 표시하기 위한 설정입니다.
+         if let popoverController = alert.popoverPresentationController {
+           popoverController.sourceView = view
+           popoverController.sourceRect = CGRect(x: view.bounds.midX, y: view.bounds.midY, width: 0, height: 0)
+           popoverController.permittedArrowDirections = [.down]
+         }
+        
+        // 액션 시트를 표시
+        present(alert, animated: true)
+    }
+    
+    private func showMessage(_ message: String) {
+        // UIAlertController를 사용해서 알림을 생성
+        let alert = UIAlertController(title: "알림", message: message, preferredStyle: .alert)
+        // "OK" 액션을 추가
+        alert.addAction(UIAlertAction(title: "확인", style: .default))
+        // 알림을 표시합니다.
+        present(alert, animated: true)
+    }
+    
     private func openLink() {
         if let url = URL(string: "https://www.apple.com"),
            UIApplication.shared.canOpenURL(url) {
-            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            UIApplication.shared.open(url) { success in
+                if success {
+                    print("웹사이트 열기 성공")
+                } else {
+                    print("웹사이트 열기 실패")
+                }
+            }
         }
     }
-
 }
 
-
-
+#Preview {
+    ViewController()
+}
