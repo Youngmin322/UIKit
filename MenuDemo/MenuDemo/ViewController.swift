497d5b5fc3a472dac69f5321392a2ceab17ea614
Youngmin Cho <choyoungmin0322@gmail.com>
2025-03-18 09:44:25 +0900

    버튼 추가 및 버튼 터치 시 링크 열기 기능 구현

--- MenuDemo/MenuDemo/ViewController.swift
+++ MenuDemo/MenuDemo/ViewController.swift
@@ -11,9 +11,53 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupUI()
+    }
+    
+    func setupUI() {
+        
+        // 디폴트 버튼 설정
+        var config = UIButton.Configuration.plain()
+        config.title = "Apple"
+        config.baseBackgroundColor = .systemBlue
+        
+        // 밑줄 스타일
+        let attributedTitle = AttributedString("String")
+        var container = AttributeContainer()
+        container.underlineStyle = .single
+        container.underlineColor = .systemBlue
+        container.font = .systemFont(ofSize: 20)
+        
+        let underlinedTitle = attributedTitle.settingAttributes(container)
+        config.attributedTitle = underlinedTitle
+        
+        // 버튼 생성
+        let button = UIButton(configuration: config)
+        button.addAction(UIAction { [weak self] _ in
+            print("버튼 클릭")
+            self?.openLink()
+        }, for: .touchUpInside)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(button)
+        
+        // 제약 조건 설정
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    private func openLink() {
+        if let url = URL(string: "https://www.apple.com"),
+           UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
     }
 
-
 }
+
+
 
