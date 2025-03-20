e7c2f6bd72eada4ff5de2d8b92a037b922383424
Youngmin Cho <choyoungmin0322@gmail.com>
2025-03-20 10:43:58 +0900

    커스텀 셀 추가 및 테이블 뷰에서 사용하도록 구성

---
+++ TableViewDemo/TableViewDemo/CustomCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomCell.swift
+//  TableViewDemo
+//
+//  Created by 조영민 on 3/20/25.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
