9c4444937b8025f78c78c3642901bce74929b45e
Youngmin Cho <choyoungmin0322@gmail.com>
2025-03-24 11:31:59 +0900

    Collection View 프로젝트 파일 생성

---
+++ CollectionViewDemo/CollectionViewDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewDemo
+//
+//  Created by 조영민 on 3/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
