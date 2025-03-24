2608f76969d3edb831d77abf654622a1ad8e3900
Youngmin Cho <choyoungmin0322@gmail.com>
2025-03-24 13:27:10 +0900

    그리드 컬렉션 뷰 셀 및 데이터 모델 추가, 컬렉션 뷰 구성 및 데이터 소스 설정

---
+++ CollectionViewDemo/CollectionViewDemo/GridCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  GridCollectionViewCell.swift
+//  CollectionViewDemo
+//
+//  Created by 조영민 on 3/24/25.
+//
+
+import UIKit
+
+class GridCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ CollectionViewDemo/CollectionViewDemo/GridItem.swift
@@ -0,0 +1,8 @@
+//
+//  GridItem.swift
+//  CollectionViewDemo
+//
+//  Created by 조영민 on 3/24/25.
+//
+
+import Foundation
