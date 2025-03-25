041e107d4f7834b6c4b795e0ced221f60207ccdf
Youngmin Cho <choyoungmin0322@gmail.com>
2025-03-25 14:08:29 +0900

    탭 바 연결

--- JRNL/JRNL/JournalListViewController.swift
+++ JRNL/JRNL/JournalListViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class JournalListViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension JournalListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "journalCell", for: indexPath)
+    }
+}
