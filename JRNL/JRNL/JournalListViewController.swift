3ea22c837534d71159a185fe98d661a7aebcc75f
Youngmin Cho <choyoungmin0322@gmail.com>
2025-03-25 14:29:16 +0900

    오류 수정 및 새로운 항목 추가 화면 및 취소 기능 구현

--- JRNL/JRNL/JournalListViewController.swift
+++ JRNL/JRNL/JournalListViewController.swift
@@ -8,20 +8,25 @@
 import UIKit
 
 class JournalListViewController: UIViewController {
-    
-    @IBOutlet weak var tableView: UITableView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+
+  @IBOutlet weak var tableView: UITableView!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  @IBAction func unwindNewEntryCancel(segue: UIStoryboardSegue) {
+    print("unwindNewEntryCancel")
+  }
 }
 
 extension JournalListViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withIdentifier: "journalCell", for: indexPath)
-    }
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return tableView.dequeueReusableCell(withIdentifier: "journalCell", for: indexPath)
+  }
 }
+
