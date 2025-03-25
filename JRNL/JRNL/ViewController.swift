2ced0b2b3719a43325babc2be64a2aef600c25d6
Youngmin Cho <choyoungmin0322@gmail.com>
2025-03-25 13:07:30 +0900

    JRNL 프로젝트 파일 생성 및 StoryBoard Tab Bar 구현

---
+++ JRNL/JRNL/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  JRNL
+//
+//  Created by 조영민 on 3/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
