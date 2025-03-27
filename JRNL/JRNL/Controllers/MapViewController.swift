7e92b8c81c469a7a190b1bfd535853bd1a9f5669
Youngmin Cho <choyoungmin0322@gmail.com>
2025-03-27 10:43:54 +0900

    MapViewController에 샘플 저널 항목 데이터 추가 및 지도에 표시 기능 구현

---
+++ JRNL/JRNL/Controllers/MapViewController.swift
@@ -0,0 +1,51 @@
+//
+//  MapViewController.swift
+//  JRNL
+//
+//  Created by 조영민 on 3/27/25.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+  @IBOutlet weak var mapView: MKMapView!
+
+  let locationManager = CLLocationManager()
+  var sampleJournalEntryData = SampleJournalEntryData()
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // 위치 정보 사용을 위한 설정
+    locationManager.delegate = self
+    locationManager.requestWhenInUseAuthorization()
+    self.navigationItem.title = "Loading..."
+    locationManager.requestLocation()
+
+    // 샘플 데이터 생성 & 지도에 표시
+    sampleJournalEntryData.createSampleJournalEntryData()
+    mapView.addAnnotations(sampleJournalEntryData.journalEntries)
+  }
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    if let location = locations.first {
+      let coordinate = location.coordinate
+      let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+      let region = MKCoordinateRegion(center: coordinate, span: span)
+      mapView.setRegion(region, animated: true)
+      self.navigationItem.title = "Map"
+    }
+  }
+
+  func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+    print("Failed to find user's location: \(error.localizedDescription)")
+  }
+}
+
+extension MapViewController: MKMapViewDelegate {
+
+}
--- JRNL/JRNL/Models/JournalEntry.swift
+++ JRNL/JRNL/Models/JournalEntry.swift
@@ -6,49 +6,66 @@
 //
 
 import UIKit
+import MapKit
 
-class JournalEntry {
-    let date: Date
-    let rating: Int
-    let entryTitle: String
-    let entryBody: String
-    let photo: UIImage?
-    let latitude: Double?
-    let longitude: Double?
-    
-    init?(rating: Int, title: String, body: String, photo: UIImage? = nil, latitude: Double? = nil, longitude: Double? = nil) {
-        if title.isEmpty || body.isEmpty || rating < 0 || rating > 5 {
-            return nil
-        }
-        self.date = Date()
-        self.rating = rating
-        self.entryTitle = title
-        self.entryBody = body
-        self.photo = photo
-        self.latitude = latitude
-        self.longitude = longitude
+class JournalEntry: NSObject, MKAnnotation {
+  let date: Date
+  let rating: Int
+  let entryTitle: String
+  let entryBody: String
+  let photo: UIImage?
+  let latitude: Double?
+  let longitude: Double?
+
+  init?(rating: Int, title: String, body: String, photo: UIImage? = nil, latitude: Double? = nil, longitude: Double? = nil) {
+    if title.isEmpty || body.isEmpty || rating < 0 || rating > 5 {
+      return nil
     }
+    self.date = Date()
+    self.rating = rating
+    self.entryTitle = title
+    self.entryBody = body
+    self.photo = photo
+    self.latitude = latitude
+    self.longitude = longitude
+  }
+
+  // MARK: - MKAnnotation
+  var title: String? {
+    date.formatted(.dateTime.month().day().year())
+  }
+
+  var subtitle: String? {
+    entryTitle
+  }
+
+  var coordinate: CLLocationCoordinate2D {
+    guard let latitude = latitude, let longitude = longitude else {
+      return CLLocationCoordinate2D()
+    }
+    return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+  }
 }
 
 struct SampleJournalEntryData {
-    var journalEntries: [JournalEntry] = []
-    
-    mutating func createSampleJournalEntryData() {
-        let photo1 = UIImage(systemName: "sun.max")
-        let photo2 = UIImage(systemName: "cloud")
-        let photo3 = UIImage(systemName: "cloud.sun")
-        guard let journalEntry1 =  JournalEntry(rating: 5, title: "Good",
-                                                body: "Today is a good day", photo: photo1) else {
-            fatalError("Unable to instantiate journalEntry1")
-        }
-        guard let journalEntry2 = JournalEntry(rating: 0, title: "Bad",
-                                               body: "Today is a bad day", photo: photo2) else {
-            fatalError("Unable to instantiate journalEntry2")
-        }
-        guard let journalEntry3 = JournalEntry(rating: 3, title: "Ok",
-                                               body: "Today is an Ok day", photo: photo3) else {
-            fatalError("Unable to instantiate journalEntry3")
-        }
-        journalEntries += [journalEntry1, journalEntry2, journalEntry3]
+  var journalEntries: [JournalEntry] = []
+
+  mutating func createSampleJournalEntryData() {
+    let photo1 = UIImage(systemName: "sun.max")
+    let photo2 = UIImage(systemName: "cloud")
+    let photo3 = UIImage(systemName: "cloud.sun")
+    guard let journalEntry1 =  JournalEntry(rating: 5, title: "Good",
+                                            body: "Today is a good day", photo: photo1) else {
+      fatalError("Unable to instantiate journalEntry1")
     }
+    guard let journalEntry2 = JournalEntry(rating: 0, title: "Bad",
+                                           body: "Today is a bad day", photo: photo2, latitude: 37.3318, longitude: -122.0312) else {
+      fatalError("Unable to instantiate journalEntry2")
+    }
+    guard let journalEntry3 = JournalEntry(rating: 3, title: "Ok",
+                                           body: "Today is an Ok day", photo: photo3) else {
+      fatalError("Unable to instantiate journalEntry3")
+    }
+    journalEntries += [journalEntry1, journalEntry2, journalEntry3]
+  }
 }
