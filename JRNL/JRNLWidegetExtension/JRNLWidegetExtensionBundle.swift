17b52696683a398ceb92b2e3f7db18d0a5c8a149
Youngmin Cho <choyoungmin0322@gmail.com>
2025-04-02 10:21:18 +0900

    WidegetExtension 추가

---
+++ JRNL/JRNLWidegetExtension/JRNLWidegetExtensionBundle.swift
@@ -0,0 +1,16 @@
+//
+//  JRNLWidegetExtensionBundle.swift
+//  JRNLWidegetExtension
+//
+//  Created by 조영민 on 4/2/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct JRNLWidegetExtensionBundle: WidgetBundle {
+    var body: some Widget {
+        JRNLWidegetExtension()
+    }
+}
